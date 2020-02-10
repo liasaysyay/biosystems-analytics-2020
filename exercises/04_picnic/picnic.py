d56843cac8b569a15d7b4f9b7358ede8ecd20fd0
Lia Crocker <liacrocker@email.arizona.edu>
2020-02-10 10:21:35 -0700

    Version 1

---
+++ exercises/04_picnic/picnic.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""
+Author : lia
+Date   : 2020-02-05
+Purpose: 	Store one or more positional arguments in a list
+            Count the number of arguments
+            Accept a --sorted argument
+            Print a statement that lists the arguments with appropriate grammar
+
+"""
+
+import argparse
+import os
+import sys
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Picnic game',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('items',
+                        metavar='str',
+                        nargs='+',
+                        help='Item(s) to bring')
+
+    parser.add_argument('-s',
+                        '--sorted',
+                        help='Sort the items',
+                        action='store_true')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Print the picnic statement with commas and 'and' before the last item"""
+
+    args = get_args()
+    items = args.items
+
+    if args.sorted:
+        items.sort()
+
+    bringing = ''
+
+    if len(items) == 1:
+        bringing = items[0]
+    elif len(items) == 2:
+        bringing = ' and ' .join(items)
+    else:
+        items[-1] = 'and ' + items[-1]
+        bringing = ', ' .join(items)
+
+    print('You are bringing {}.'.format(bringing))
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
