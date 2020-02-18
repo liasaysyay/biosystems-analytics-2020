2ea2fc49e15cc46244bb4ff0b1df88362eab9b34
Lia Crocker <liacrocker@email.arizona.edu>
2020-02-17 21:13:12 -0700

    Version 1

---
+++ exercises/05_jump_the_five/jump_01.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""
+Author : lia
+Date   : 2020-02-12
+Purpose: 	Create a dictionary
+            Use a for loop to process text character-by-character
+            Print a new string with the associated numbers of the input
+
+"""
+
+import argparse
+import os
+import sys
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Jump the Five',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('text',
+                        metavar='str',
+                        help='Input text')
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    text = args.text
+
+    jumper = {'1': '9',
+              '2': '8',
+              '3': '7',
+              '4': '6',
+              '5': '0',
+              '6': '4',
+              '7': '3',
+              '8': '2',
+              '9': '1',
+              '0': '5'}
+
+    for char in text:
+        print(jumper.get(char, char), end='')
+
+    print()
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
