17d43e0cccb2018aea23ab251700998c65019c96
Lia Crocker <liacrocker@email.arizona.edu>
2020-02-25 22:17:30 -0700

    Version 2

--- exercises/07_wc/wc.py
+++ exercises/07_wc/wc.py
@@ -2,7 +2,10 @@
 """
 Author : lia
 Date   : 2020-02-20
-Purpose: Rock the Casbah
+Purpose: Count the characters, words and lines for all the files given as positional arguments
+        Print the number of lines, words and characters followed by the file name
+        Print the totals when given multiple file
+        Read from standard in --> STDIN
 """
 
 import argparse
@@ -33,10 +36,10 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    num_lines, num_words, num_chars = 0, 0, 0
     total_lines, total_words, total_chars = 0, 0, 0
 
     for fh in args.file:
+        num_lines, num_words, num_chars = 0, 0, 0
         for line in fh:
             num_lines += 1
             num_words += len(line.split())
@@ -50,6 +53,8 @@ def main():
 
     if len(args.file) > 1:
         print(f'{total_lines:8}{total_words:8}{total_chars:8} total')
+
+    print(type(line))
 
 
 # --------------------------------------------------
