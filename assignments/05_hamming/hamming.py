eb3e523f249e0f819f417dce9ef42b418fc3b5ea
Lia Crocker <liacrocker@email.arizona.edu>
2020-03-05 10:26:47 -0700

    Version 2

--- assignments/05_hamming/hamming.py
+++ assignments/05_hamming/hamming.py
@@ -41,18 +41,18 @@ def main():
 
     args = get_args()
 
-    for fh in args.file:
-        num_words = len(fh.split())
+    for line in args.file:
+        num_words = len(line.split())
         num_pairs = int(num_words / 2)
         for i in range(num_words - num_pairs):
-            word1, word2 = fh.split()
+            word1, word2 = line.split()
             ham = [c1 == c2 for c1, c2 in zip(word1, word2)]
             hamming = len(word2) - (sum(ham[:]))
 
             if hamming >= args.min:
                 print(f'{hamming:8}:{word1:20}{word2:20}')
 
-    
+
 # --------------------------------------------------
 if __name__ == '__main__':
     main()
